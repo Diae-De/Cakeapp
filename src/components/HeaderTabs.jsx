9b0fec47b00f26834d3c5f5436478204ad8ca345
Dyae Pr <diaeprsup@gmail.com>
2022-02-26 12:53:29 +0100

    chages

--- src/components/HeaderTabs.jsx
+++ src/components/HeaderTabs.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./Headerstyle.css"
 import image from "../images/icon.jpg"
 import { BsListNested } from "react-icons/bs";
@@ -6,6 +6,11 @@ import {HiShoppingCart} from "react-icons/hi"
 import Fade from 'react-reveal/Fade'
 
 export default function HeaderTabs() {
+  const [activenav,setActivenav] = useState(false)
+
+  function toggle(){
+    setActivenav(!activenav)
+  }
   return (
     <>
     <Fade top>
@@ -16,7 +21,14 @@ export default function HeaderTabs() {
             <div className="todaysdo">What would like to do!</div>
         </div>
         <div className="carticon"><HiShoppingCart/></div>
-        <div className="listicon"><BsListNested/></div>
+        <div className="listicon" onClick={toggle}><BsListNested/></div>
+        <div className="navbar">
+          <div className={activenav ? "navbaropen" : "navbarclose"}>      
+          <li>Home</li>
+          <li>About</li>
+          <li>Learn More</li>
+          </div> 
+        </div>
     </div>
     </Fade>
         
